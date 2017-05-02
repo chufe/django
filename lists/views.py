d6a8bcfe7107c3e66b15dc531092fc8ece1a000e
chufe <chufen.hi@163.com>
2017-05-02 14:20:26 +0800

    测试版V1.0

---
+++ lists/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+
+from lists.models import Item, List
+
+# Create your views here.
+
+def home_page(request):
+    """
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+    
+    return render(request, 'home.html',{
+            'new_item_text': new_item_text
+        })
+    """
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
+    # items = Item.objects.all()
+    return render(request, 'home.html')
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
